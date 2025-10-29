35d9bd7dc7cc2c311803a0de67494cb5d3b1a5ff
raveron689-cpu <raveron689@gmail.com>
2025-10-29 11:48:38 +0300

    Add files via upload

---
+++ First Project.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	int nums[5][10];
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 10; j++) {
+			int randNum = 0 + rand() % 25;
+			nums[i][j] = randNum;
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 10; j++) {
+			cout << nums[i][j];
+			cout << "   ";
+		}
+		cout << endl;
+	}
+
+	return 1;
+}
+
+
+
+
+
